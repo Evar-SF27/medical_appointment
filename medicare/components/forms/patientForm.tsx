9aa44c195d3e96bc2558899309390ecf34ed004b
Evar-SF27 <evarfomuso@outlook.com>
2024-07-19 21:31:48 +0100

    patient form

--- medicare/components/forms/patientForm.tsx
+++ medicare/components/forms/patientForm.tsx
@@ -1,11 +1,77 @@
-import React from 'react'
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../shared/customFormField"
+import { FormFieldType } from "@/enums/formFieldType"
+import SubmitButton from "../shared/submitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+// import { useRouter } from "next/router"
+ 
+export default function PatientForm() {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    // const router = useRouter();
+  
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+        name: "",
+        email: "",
+        phone: "",
+        },
+    })
+ 
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true)
 
-const PatientForm = () => {
+        try {
+            const userData = { name, email, phone}
+            console.log(userData)
+
+            // const user = await createUser(userData)
+            // if(user) router.push(`/patients/${user.$id}/register`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
   return (
-    <div>
-      PAteint Foem
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+            <h1 className="header">Hi there 👋🏼</h1>
+            <p className="text-dark-700">Schedule your first appointment</p>
+        </section>
+        <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="name"
+            label="Full Name"
+            placeholder="John Doe"
+            iconSrc="/assets/icons/user.svg"
+            iconAlt="user"
+        />
+        <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="johndoe@gmail.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="user"
+        />
+        <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="phone"
+            label="Phone Number"
+            placeholder="(237) 688 435 345"
+        />
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
   )
 }
-
-export default PatientForm
